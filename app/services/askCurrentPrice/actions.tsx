0c839badfa7ed7203bfa88d84d9aa36b8942a0e6
Goldfinch <facu_vila@hotmail.com>
2025-02-08 03:55:23 -0300

    se parseo

--- app/services/askCurrentPrice/actions.tsx
+++ app/services/askCurrentPrice/actions.tsx
@@ -1,6 +1,7 @@
 'use server'
 import {AskCurrentPrice, parseDateToString} from "@/lib/utils";
 import {addDays, INTERVALO_DE_DIAS} from "@/lib/utils";
+import moment from 'moment';
 
 
 export async function getCurrentPrice (values: AskCurrentPrice): Promise<number> {
@@ -8,10 +9,15 @@ export async function getCurrentPrice (values: AskCurrentPrice): Promise<number>
     const baseDate = new Date(parseDateToString(values.oldDate))
     const to = addDays(baseDate, INTERVALO_DE_DIAS)
     const from = addDays(baseDate, -INTERVALO_DE_DIAS)
+    
+    const toString = moment(to).format('DD-MM-YYYY')
+    const fromString = moment(from).format('DD-MM-YYYY');
+    
 
-
-
-    const result = await fetch(`https://mercados.ambito.com//dolar/informal/historico-general/${}/${}`);
-    console.log("Calculando precio actual...");
+    const result = await fetch(`https://mercados.ambito.com//dolar/informal/historico-general/${fromString}/${toString}`);
+    console.log(`https://mercados.ambito.com//dolar/informal/historico-general/${fromString}/${toString}`);
+    const data = await result.json();
+    const dataSlice = data.slice(1).map((element: string[]) => (parseFloat(element[1]) + parseFloat(element[2]) / 2 ) );
+    console.log(dataSlice)
     return 4;
 }
